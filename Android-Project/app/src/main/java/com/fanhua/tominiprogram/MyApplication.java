8987db7d6260884bd8e067c2e37944e50828befe
yanxiaosa <15555994291@163.com>
2018-10-29 11:29:53 +0800

    提交代码

--- Android-Project/app/src/main/java/com/fanhua/tominiprogram/MyApplication.java
+++ Android-Project/app/src/main/java/com/fanhua/tominiprogram/MyApplication.java
@@ -1,5 +1,8 @@
 package com.fanhua.tominiprogram;
 
+
+import android.content.pm.ActivityInfo;
+
 import com.fanhua.uiadapter.LibApplication;
 import com.fanhua.uiadapter.Resolution;
 
@@ -7,6 +10,6 @@ public class MyApplication extends LibApplication {
 
     @Override
     public Resolution designResolutionForApp() {
-        return new Resolution(750, 1334, 1.0F, 160, 1);
+        return new Resolution(750, 1334, 1.0F, 160, ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
     }
 }
